6ab59187db6d618c4952f45488e8ca41e5ea7eb4
Mateogarcia666 <mateoriver1@gmail.com>
2024-10-08 02:55:29 +0000

    ADD: arreglo de un error de particiones

--- RTA_SCRIPT_Examen_20241007/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241007/Punto_B.sh
@@ -53,26 +53,26 @@ sudo mkfs  -t ext4 /dev/sdc8
 sudo mkfs  -t ext4 /dev/sdc9
 sudo mkfs  -t ext4 /dev/sdc10
 sudo mkfs  -t ext4 /dev/sdc11
-sudo mount /dev/sdc1 /Examenes-UTN/alumno_1/parcial_1
-sudo mount /dev/sdc2 /Examenes-UTN/alumno_1/parcial_2
-sudo mount /dev/sdc3 /Examenes-UTN/alumno_1/parcial_3
-sudo mount /dev/sdc5 /Examenes-UTN/alumno_2/parcial_1
-sudo mount /dev/sdc6 /Examenes-UTN/alumno_2/parcial_2
-sudo mount /dev/sdc7 /Examenes-UTN/alumno_2/parcial_3
-sudo mount /dev/sdc8 /Examenes-UTN/alumno_3/parcial_1
-sudo mount /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2
-sudo mount /dev/sdc10 /Examenes-UTN/alumno_3/parcial_3
+sudo mount /dev/sdc1 /Examenes-UTN/alumno_1/parcial1
+sudo mount /dev/sdc2 /Examenes-UTN/alumno_1/parcial2
+sudo mount /dev/sdc3 /Examenes-UTN/alumno_1/parcial3
+sudo mount /dev/sdc5 /Examenes-UTN/alumno_2/parcial1
+sudo mount /dev/sdc6 /Examenes-UTN/alumno_2/parcial2
+sudo mount /dev/sdc7 /Examenes-UTN/alumno_2/parcial3
+sudo mount /dev/sdc8 /Examenes-UTN/alumno_3/parcial1
+sudo mount /dev/sdc9 /Examenes-UTN/alumno_3/parcial2
+sudo mount /dev/sdc10 /Examenes-UTN/alumno_3/parcial3
 sudo mount /dev/sdc11 /Examenes-UTN/profesores
 
-echo "/dev/sdc1 /Examenes-UTN/alumno_1/parcial_1  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc2 /Examenes-UTN/alumno_1/parcial_2  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc3 /Examenes-UTN/alumno_1/parcial_3  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc5 /Examenes-UTN/alumno_2/parcial_1  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc6 /Examenes-UTN/alumno_2/parcial_2  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc7 /Examenes-UTN/alumno_2/parcial_3  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc8 /Examenes-UTN/alumno_3/parcial_1  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc9 /Examenes-UTN/alumno_3/parcial_2  ext4 default 0 0" | sudo tee -a /etc/fstab
-echo "/dev/sdc10 /Examenes-UTN/alumno_3/parcial_3  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc1 /Examenes-UTN/alumno_1/parcial1  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc2 /Examenes-UTN/alumno_1/parcial2  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc3 /Examenes-UTN/alumno_1/parcial3  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc5 /Examenes-UTN/alumno_2/parcial1  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc6 /Examenes-UTN/alumno_2/parcial2  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc7 /Examenes-UTN/alumno_2/parcial3  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc8 /Examenes-UTN/alumno_3/parcial1  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc9 /Examenes-UTN/alumno_3/parcial2  ext4 default 0 0" | sudo tee -a /etc/fstab
+echo "/dev/sdc10 /Examenes-UTN/alumno_3/parcial3  ext4 default 0 0" | sudo tee -a /etc/fstab
 echo "/dev/sdc11 /Examenes-UTN/profesores  ext4 default 0 0" | sudo tee -a /etc/fstab
 sudo mount -a
 sudo cat /etc/fstab
